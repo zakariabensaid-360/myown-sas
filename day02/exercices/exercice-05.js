31422eb3db288bbdb8fc2b7f87803fdb207ab781
Zakaria Bensaid <zakariabensaid58@gmail.com>
2026-09-08 18:12:30 +0200

    solved the exc

--- day02/exercices/exercice-05.js
+++ day02/exercices/exercice-05.js
@@ -14,4 +14,8 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let password = "faux"
+do {
+    console.log("Veuillez entrer le mot de passe")
+    password = "secret"
+} while (password == "faux");

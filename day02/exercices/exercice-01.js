bb6246931af04e0fbf16b44393a348378d7a6104
Zakaria Bensaid <zakariabensaid58@gmail.com>
2026-09-08 17:35:06 +0200

    added the answer

--- day02/exercices/exercice-01.js
+++ day02/exercices/exercice-01.js
@@ -15,3 +15,7 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+for (let i = 0; i < 10; i++) {
+    console.log("Décollage !")
+}

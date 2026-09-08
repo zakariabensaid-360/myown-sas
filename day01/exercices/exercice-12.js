fb0f0f30b26331168b7a13348cdbc31e74135747
Zakaria Bensaid <zakariabensaid58@gmail.com>
2026-09-08 15:45:47 +0200

    added .  by the coach to bypass the syntax error

--- day01/exercices/exercice-12.js
+++ day01/exercices/exercice-12.js
@@ -21,7 +21,7 @@
 const annee = 2024
 
 if(annee % 4 == 0 && annee % 400 != 0 && annee % 100 != 0) {
-    console.log("2024 est une année bissextile")
+    console.log("2024 est une année bissextile.")
 } else {
     return;
 }

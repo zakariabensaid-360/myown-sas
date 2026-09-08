c77e5bd58d9adf2806c02287d65c666c06a2a68e
Zakaria Bensaid <zakariabensaid58@gmail.com>
2026-09-08 18:05:10 +0200

    solved the exc

--- day02/exercices/exercice-04.js
+++ day02/exercices/exercice-04.js
@@ -17,4 +17,8 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let temp = 0
+for (let i = 0; i <= 50; i++) {
+    temp += i 
+}
+console.log(temp)

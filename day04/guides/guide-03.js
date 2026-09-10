b089c825a23233e730fce47ece0d6e215451016c
Zakaria Bensaid <zakariabensaid58@gmail.com>
2026-09-10 10:26:59 +0200

    solving the guides

---
+++ day04/guides/guide-03.js
@@ -0,0 +1,15 @@
+/**
+ * JOUR 03 — GUIDE 03
+ * La syntaxe moderne (Fonction Fléchée)
+ *
+ * OBJECTIF
+ * Transformez la fonction classique function carre(x) { return x * x; } en sa version la plus courte en fonction fléchée.
+ *
+ * Aide : consulte ../02-guides.md seulement si tu bloques.
+ */
+'use strict';
+
+const arrowfun = (x) => x * x
+
+
+console.log(arrowfun(2))

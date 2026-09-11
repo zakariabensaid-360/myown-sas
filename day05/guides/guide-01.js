d862ffb2378dc04e19b2d8d1af2dfc38d11ff50f
Zakaria Bensaid <zakariabensaid58@gmail.com>
2026-09-11 06:27:52 -0200

    solving guides

---
+++ day05/guides/guide-01.js
@@ -0,0 +1,15 @@
+/**
+ * JOUR 05 — GUIDE 01
+ * Extraire une sous-chaîne
+ *
+ * OBJECTIF
+ * On vous donne une date au format ISO "2024-10-15". Récupérez uniquement l'année.
+ *
+ * Aide : consulte ../02-guides.md seulement si tu bloques.
+ */
+'use strict';
+
+
+const values = "2024-10-15".split('-')
+
+console.log(values[0])
